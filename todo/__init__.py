32ba3d2e3f5478279fe08fa103818533ac1432a5
Aníbal Álvarez <aalvarez@miningtag.cl>
2021-01-16 13:26:38 -0300

    Init app and config db

---
+++ todo/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import os
+
+def create_app(): 
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY='mikey',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
+
+
+    from . import db
+
+    db.init_app(app)
+
+    @app.route('/hola')
+    def hola(): 
+        return 'Hola mundo!'
+    
+    return app
